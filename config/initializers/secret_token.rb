e6761a814148e06dd884ed23705f62cd3140c7db
ma2ge <takayuki.1229@gmail.com>
2012-07-01 10:29:27 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Fakeb::Application.config.secret_token = '858f6baeb75e7666f38f3a43409aeb951f1903c3244b7198d6e6a769420c4673647ae0faf21a562a296e98aafc2f0edca86d96f246a970a4418acbbda4575677'
