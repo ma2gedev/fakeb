1b61ffbc377bae242a697d85c76778ca35409630
ma2ge <takayuki.1229@gmail.com>
2012-07-16 00:32:09 +0900

    adjust for heroku shared database restriction
    I caught error described in the following
    See Also: http://stackoverflow.com/questions/10066041/heroku-pgerror-error-column-must-appear-in-the-group-by-clause-or-be-used-i

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,10 @@
 class TopController < ApplicationController
   def index
-    @entries = Entry.select("entries.title, entries.url, count(*) AS count").joins(:bookmarks).group("bookmarks.entry_id").order("count desc")
+    # BAD way, because heroku shared database has bugs that fixed by PostgreSQL 9.1
+    # See Also: http://stackoverflow.com/questions/10066041/heroku-pgerror-error-column-must-appear-in-the-group-by-clause-or-be-used-i
+    # should be the following code commented out
+    # @entries = Entry.select("entries.title, entries.url, count(*) AS count").joins(:bookmarks).
+    #              group("bookmarks.entry_id").order("count desc")
+    @bookmarks = Bookmark.select("entry_id, count(*) AS count").group("entry_id").order("count desc").includes(:entry)
   end
 end
